255beb18571097633165eaa468d7f7bfe1c85bb7
touqeer25 <touqeerahmad25@gmail.com>
2026-09-21 18:10:06 +0500

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bismillah Clinic - Service Worker (robust offline cache)
-const CACHE_NAME = 'bhc-clinic-v52';
+const CACHE_NAME = 'bhc-clinic-v53';
 
 // Keep this list same-origin and reliable. Missing/external files are not allowed
 // to break the whole install anymore.
